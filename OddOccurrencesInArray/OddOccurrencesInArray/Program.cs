0c24d9f4f2317aafab60242924ef7d36cd5aca8d
Gustavo Almeida <gustatec@outlook.com>
2019-05-22 22:46:31 -0300

    Add OddOccurrencesInArray

---
+++ OddOccurrencesInArray/OddOccurrencesInArray/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace OddOccurrencesInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var aox = solution(new int[] { 1, 1, 1, 1, 1, 1, 1, 2, 2, 4, 4, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8 });
+        }
+
+        public static int solution(int[] A) => A.GroupBy(x => x).FirstOrDefault(x => x.Count() % 2 > 0).Key;
+    }
+}
