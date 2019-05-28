a6859bb7c4d5e8b07056d7d6d25ad2463fc31ba7
Gustavo Almeida <gustatec@outlook.com>
2019-05-27 22:47:52 -0300

    FrogJump exercise

---
+++ FrogJmp/FrogJmp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FrogJmp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = solution(10, 100, 10);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        public static int solution(int X, int Y, int D)
+        {
+            var distanceToJump = Y - X;
+
+            if (distanceToJump <= 0)
+                return 0;
+
+            var jumpsNecessary = (double)distanceToJump / D;
+            return Convert.ToInt32(Math.Ceiling(jumpsNecessary));
+        }
+    }
+}
