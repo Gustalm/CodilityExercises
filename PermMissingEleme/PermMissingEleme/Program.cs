5c007b820f6ef9c75ab1406bb484c9444cfa61ae
Gustavo Almeida <gustatec@outlook.com>
2019-06-03 22:47:34 -0300

    PermMIssingElement 100%

--- PermMissingEleme/PermMissingEleme/Program.cs
+++ PermMissingEleme/PermMissingEleme/Program.cs
@@ -14,18 +14,24 @@ namespace PermMissingEleme
 
         public static int solution(int[] A)
         {
-            var ordered = A.OrderBy(x => x).ToList();
+            var sum = 0;
+            for (int i = 0; i < A.Length; i++)
+                sum += A[i];
 
-            if (ordered.Count == 1 && ordered[0] == 2)
-                return 1;
+            return A.Length % 2 == 0 ? -sum + (A.Length / 2 + 1) * (A.Length + 1)
+             : -sum + (A.Length / 2 + 1) * (A.Length + 2);
+            //var ordered = A.OrderBy(x => x).ToList();
 
-            for (int i = 0; i < ordered.Count() - 1; i++)
-            {
-                if (ordered[i] + 1 != ordered[i + 1])
-                    return ordered[i] + 1;
-            }
+            //if (ordered.Count == 1 && ordered[0] == 2)
+            //    return 1;
 
-            return 0;
+            //for (int i = 0; i < ordered.Count() - 1; i++)
+            //{
+            //    if (ordered[i] + 1 != ordered[i + 1])
+            //        return ordered[i] + 1;
+            //}
+
+            //return 0;
         }
     }
 }
