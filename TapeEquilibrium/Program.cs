058e43b25fd45576c4165383c2c22857b708a573
Gustavo Almeida <gustatec@outlook.com>
2019-07-04 21:33:37 -0300

    TapeEquilibrium firstpart

---
+++ TapeEquilibrium/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TapeEquilibrium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = solution(new int[] { 3,1,2,4,3 });
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        public static int solution(int[] A)
+        {
+            var dic = new Dictionary<int, int>();
+
+            for (int i = 0; i < A.Length -1; i++)
+            {
+                if (i == 0)
+                    continue;
+
+                dic.Add(A[i], dic.Sum(x => x.Value) + A[i-1]);
+            }
+
+            return dic.Min(x => x.Value);
+        }
+    }
+}
